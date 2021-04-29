779b1ff7bac442180570fb7da88a40851c4c7c13
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 16:56:09 +0530

    Update gpio.h

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -1,7 +1,18 @@
+/**
+ * @file gpio.h
+ * @author Archisa Kalyani
+ * @brief Header file for defining gpio() function
+ * @version 0.1
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #ifndef GPIO_H_INCLUDED
 #define GPIO_H_INCLUDED
 
-gpio();
+ void gpio();
 
-#endif // GPIO_H_INCLUDED
+#endif /** GPIO_H_INCLUDED*/
 
