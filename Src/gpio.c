b5ba3b706825e5003b2983dbf49dae97cf89a4f8
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 16:50:56 +0530

    Update gpio.c

--- Src/gpio.c
+++ Src/gpio.c
@@ -1,13 +1,24 @@
+/**
+ * @file gpio.c
+ * @author Archisa Kalyani
+ * @brief source file for setting port direction
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include<avr/io.h>
 void gpio()
 {
-  //setting direction of ports
-  DDRB|=(1<<PB0); // set b0=1 for led
-  DDRD&=~(1<<PD0); // clear bit
-  DDRD&=~(1<<PD1); // clear bit
+  /**setting direction of ports*/
+  DDRB|=(1<<PB0); /**set b0=1 for led*/
+  DDRD&=~(1<<PD0); /** clear bit*/
+  DDRD&=~(1<<PD1); /** clear bit*/
 
-  // input for push buttons, pull-up +5V
+  /** input for push buttons, pull-up +5V*/
   PORTD|=(1<<PD0);
   PORTD|=(1<<PD1);
 
 }
+
