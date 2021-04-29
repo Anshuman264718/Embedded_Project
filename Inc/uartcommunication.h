4652d468346e786da718020b43248f47972300fb
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 16:59:07 +0530

    Add files via upload

---
+++ Inc/uartcommunication.h
@@ -0,0 +1,8 @@
+#ifndef UARTCOMMUNICATION_H_INCLUDED
+#define UARTCOMMUNICATION_H_INCLUDED
+
+void UARTinit(uint16_t);
+char UARTreadchar();
+void UARTwritecharacter(char);
+
+#endif // UARTCOMMUNICATION_H_INCLUDED
