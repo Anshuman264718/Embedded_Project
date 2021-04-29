0603304576ea5f04b880414bf6b60d99bd680c8a
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 16:52:29 +0530

    Update adc.c

--- Src/adc.c
+++ Src/adc.c
@@ -1,18 +1,43 @@
+
+/**
+ * @file adc.c
+ * @author Archisa Kalyani
+ * @brief source file to implement ADC function for temperature sensor
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
 #include<avr/io.h>
+/** \brief
+ function to initialize ADC
+ */
+
 void InitADC()
 {
-    ADMUX|=(1<<REFS0); // SET Aref =AVcc= +5V
-    ADCSRA|=(1<<ADEN)|(7<<ADPS0); //enable ADC and setting prescalar value of 128 fcpu
+    ADMUX|=(1<<REFS0); /** SET Aref =AVcc= +5V*/
+    ADCSRA|=(1<<ADEN)|(7<<ADPS0); /**enable ADC and setting prescalar value of 128 fcpu*/
 }
+/** \brief this function receives the temperature values from the temperature sensor and stores
+  output in 2 8-bit ADC register(ADCL AND ADCH)
+* \param ch
+ * \return ADC to uint16_t
+ *
+ */
+
 uint16_t ReadADC(uint8_t ch)
         {
-          // select channel ch for ADC
+          /** select channel ch for ADC*/
           ADMUX&=0xf8;
           ch=ch&0b00000111;
           ADMUX|=ch;
-          ADCSRA|=(1<<ADSC); // to start single conversion
-          while(!(ADCSRA & (1<<ADIF))); // wait for conversion to complete
+          /** to start single conversion*/
+          ADCSRA|=(1<<ADSC);
+          /** wait for conversion to complete*/
+          while(!(ADCSRA & (1<<ADIF)));
           ADCSRA|=(1<<ADIF);
           return(ADC);
 
         }
+
