42d498f592c297915e5dfe045ba10939d8efbdb3
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 17:05:06 +0530

    Update SeatHeatingApp.c

--- SeatHeatingApp.c
+++ SeatHeatingApp.c
@@ -1,38 +1,47 @@
-
-
+/**
+ * @file SeatHeatingApp.c
+ * @author Archisa Kalyani
+ * @brief main source file executing the entire program
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include<avr/io.h>
 #include<util/delay.h>
 #define sw1 !(PIND&(1<<PD0)) /** defining macros for pin DO and D1 */
 #define sw2 !(PIND&(1<<PD1)) /**and D1 */
-
-
-int SeatHeatingApp(void)
-
+#include "gpio.h"
+#include "adc.h"
+#include "pwm.h"
+#include "uartcommunication.h"
+int main(void)
 {
    gpio(); /**function call for setting ports*/
-   InitADC(); /**function call for initializing ADC*/
-   pwm(); /**function call for executing PWM*/
+   UARTinit(103); /**Initialise UART registers*/
+   int temp_data;
 
     while(1)
     {
         if((sw1==1)&(sw2==1)) /**checking input from button sensor*/
         {
+            InitADC(); /**function call for initializing ADC*/
+          pwm(); /**function call for executing PWM*/
+
             PORTB|=(1<<PB0); /**led turns on*/
             _delay_ms(200);
+            uint16_t temp=ReadADC(0); /**calling channel 0*/
+            _delay_ms(200);
+            temp_data=PWMoutput(temp);
+            UARTwritecharacter(temp_data);
+
         }
         else
         {
             PORTB&=~(1<<PB0); /** led turns off*/
+            OCR1A=0;
             _delay_ms(200);
         }
-
-       uint16_t temp=ReadADC(0); /**calling channel 0*/
-        _delay_ms(200);
-
-        /** the function gives ADC values as input for PWM*/
-        OCR1A= ReadADC(0) ;
-        _delay_ms(2000);
-
     }
     return 0;
 }
