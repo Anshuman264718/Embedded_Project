ee6c5c175a2b903f7ab5dccc8b2b72ecdbd30c46
Archisa264793 <80762968+Archisa264793@users.noreply.github.com>
2021-04-29 16:56:56 +0530

    Update adc.h

--- Inc/adc.h
+++ Inc/adc.h
@@ -1,7 +1,17 @@
+/**
+ * @file adc.h
+ * @author Archisa Kalyani
+ * @brief Header file for defining ADC() function and ReadADC() fucntion
+ *
+ * @date 2021-04-26
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #ifndef ADC_H_INCLUDED
 #define ADC_H_INCLUDED
 
-InitADC();
-ReadADC(uint8_t ch);
+void InitADC();
+uint16_t ReadADC(uint8_t);
 
-#endif // ADC_H_INCLUDED
+#endif /** ADC_H_INCLUDED*/
